e691fd2f43d35d81319cdd9c7282f7346e6961ec
devbrandelli <febrandelli.dev@gmail.com>
2020-04-25 13:19:11 -0300

    teste4

--- src/Controllers/LoginController.js
+++ src/Controllers/LoginController.js
@@ -1,6 +1,6 @@
 const User = require('../Models/user')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonWebToken')
+const jwt = require('jsonwebtoken')
 require('dotenv/config')
 
 module.exports = {
